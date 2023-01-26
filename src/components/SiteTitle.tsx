c37eefdfe4ccce99d99e3901d732b7af78ee039d
cunzaizhuyi <877824709@qq.com>
2023-01-26 15:07:01 +0800

    feat: 加GitHub

--- src/components/SiteTitle.tsx
+++ src/components/SiteTitle.tsx
@@ -2,7 +2,7 @@ import { component$ } from '@builder.io/qwik';
 
 export default component$(() => {
   return (
-    <scetion className='centerLayout'>
+    <scetion className='centerLayout relative'>
       <div class='text-center'>
         <h1 class=''>
           <div class='i-mdi-forest text-green'></div>
@@ -10,6 +10,9 @@ export default component$(() => {
           <div className='i-mdi-forest text-green'></div>
         </h1>
         <div className='slogan'>前端酷站 尽收眼底</div>
+      </div>
+      <div class='absolute right-20px top--10px'>
+        <a href="https://github.com/cunzaizhuyi/awesome-fe-sites" target='_blank'>github</a>
       </div>
     </scetion>
   )
