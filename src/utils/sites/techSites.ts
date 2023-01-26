f49301d6976823ac44d5368c8ec3bdff4c6180da
cunzaizhuyi <877824709@qq.com>
2023-01-26 18:35:00 +0800

    feat: web.dev

--- src/utils/sites/techSites.ts
+++ src/utils/sites/techSites.ts
@@ -4,17 +4,22 @@ export const techSites = {
     {
       name: '掘金',
       link: 'https://juejin.cn/',
-      desc: '',
+      desc: '中文技术社区',
     },
     {
       name: 'segmentfault',
       link: 'https://segmentfault.com/',
-      desc: '',
+      desc: '中文技术问答社区',
     },
     {
       name: 'v2ex',
       link: 'https://www.v2ex.com/',
       desc: '',
+    },
+    {
+      name: 'web.dev',
+      link: 'https://web.dev/blog/',
+      desc: 'Chrome DevRel 团队的博客'
     },
     {
       name: 'hive.one',
