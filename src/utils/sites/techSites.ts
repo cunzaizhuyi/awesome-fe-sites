8437f99595a3be36280fc6e5f0f1ffb24f655138
cunzaizhuyi <877824709@qq.com>
2023-01-28 11:22:33 +0800

    feat: 增加dev.to

--- src/utils/sites/techSites.ts
+++ src/utils/sites/techSites.ts
@@ -2,6 +2,11 @@ export const techSites = {
   type: '技术站点',
   sites: [
     {
+      name: 'dev.to',
+      link: 'https://dev.to/',
+      desc: '拥有百万开发者的技术社区，开发者分享交流，获取最新技术资讯，提高专业能力的好地方'
+    },
+    {
       name: '掘金',
       link: 'https://juejin.cn/',
       desc: '中文技术社区',
