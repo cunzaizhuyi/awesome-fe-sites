ce9508f1d9f6b197fa6fed68c8bd55370cb74605
cunzaizhuyi <877824709@qq.com>
2023-01-28 09:58:18 +0800

    feat: 加publint

--- src/utils/sites/ossSites.ts
+++ src/utils/sites/ossSites.ts
@@ -7,6 +7,11 @@ export const ossSites = {
       name: 'bestofjs',
       link: 'https://bestofjs.org/',
       desc: '一个关于前端开源项目最新最热趋势的站点',
-    }
+    },
+    {
+      name: 'publint',
+      link: 'https://publint.dev/',
+      desc: '检查一个npm包是否发布正确，作者是vite和svelte成员'
+    },
   ],
 }
