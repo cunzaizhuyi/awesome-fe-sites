cb1d0bb278999a2966b0e09a6044b95eedde12a5
cunzaizhuyi <877824709@qq.com>
2023-01-30 20:26:24 +0800

    Update blog.ts

--- src/utils/sites/blog.ts
+++ src/utils/sites/blog.ts
@@ -10,6 +10,11 @@ export const teamBlog = {
       name: 'builder.io',
       link: 'https://www.builder.io/blog',
       desc: 'builder.io公司的技术博客，CTO是angular和qwik作者'
+    },
+    {
+      name: 'chrome开发团队',
+      link: 'https://developer.chrome.com/blog/',
+      desc: 'chrome团队的博客，Chrome有什么新特性出来了，一般都会发博客',
     }
   ]
 };
