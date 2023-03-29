079028361c6ed3326486f1e211d461cf7fa69835
cunzaizhuyi <877824709@qq.com>
2023-03-29 20:52:40 +0800

    feat: 加速

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,4 +1,4 @@
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
-export const prerender = false;
+export const prerender = true;
 export const ssr = false;
