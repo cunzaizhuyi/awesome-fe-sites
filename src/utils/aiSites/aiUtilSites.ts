15a49d3ab0afbb8d85842caff4a96251536a4813
lainbo <lainbo@foxmail.com>
2023-04-09 04:14:10 +0800

    feat: ✨ Add ai navigation site

--- src/utils/aiSites/aiUtilSites.ts
+++ src/utils/aiSites/aiUtilSites.ts
@@ -54,6 +54,11 @@ export const aiUtilSites = {
       desc: '全球1240+AI工具集合导航',
     },
     {
+      name: 'AI工具百宝箱',
+      link: 'https://www.explainthis.io/zh-hans/ai-toolkit ',
+      desc: '最实用、最热门的 AI 工具，帮助你在工作与学习上更轻松有效率',
+    },
+    {
       name: 'phind',
       link: 'https://www.phind.com/',
       desc: '面向开发者的AI搜索引擎',
